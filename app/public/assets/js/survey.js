416326bf435d015f3d6c022430d6078fd869b830
Mikhail Metrikin <metrikin@gmail.com>
2017-04-28 00:53:17 -0700

    Almost completed

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -3,41 +3,47 @@
  */
 $(document).ready(()=>{
   "use strict";
-  let friends = require("../../../data/friends.js");
-  let user = {};
 
-  $("#submit-profile").on('click', ()=>{
+  $.get('/api/friends').done(response =>{
 
-    let name = $("#name").val().trim();
-    let photo = $("#photo").val().trim();
+    let friends = response;
+    let match = undefined;
+    let user = {};
 
-    if(!name || !photo){
-      //TODO: Maybe a modal added here
-      alert("Please Enter both your name and a source for an image");
-      return
-    }
+    $("#submit-profile").on('click', ()=>{
+      let name = $("#name").val().trim();
+      let photo = $("#photo").val().trim();
 
-    user.name = name;
-    user.photo = photo;
-    let answers = [];
+      if(!name || !photo){
+        //TODO: Maybe a modal added here
+        alert("Please Enter both your name and a source for an image");
+        return
+      }
 
-    try{
-      $(".chosen-select").each(()=>{
-        let option = $(this).val();
-        if(!option){
-          throw new Error("Some questions are unanswered");
-        }
-        answers.push(option);
+      user.name = name;
+      user.photo = photo;
+      let answers = [];
+
+      try{
+        $(".chosen-select").each(()=>{
+          let option = $(this).val();
+          if(!option){
+            throw new Error("Some questions are unanswered");
+          }
+          answers.push(option);
+        });
+      }catch(e){
+        //TODO: Logic to indicate that player did not answer all q's
+        alert("Please answer all of the questions");
+        return
+      }
+
+      user.scores = answers;
+
+      $.post('/api/friends', user).done(response=>{
+        match = response;
+        // TODO: Build the modal and show it
       });
-    }catch(e){
-      //TODO: Logic to indicate that player did not answer all q's
-      alert("Please answer all of the qestions");
-      return
-    }
-
-    user.scores = answers;
-
-    $.post('/api/friends', user).done(response=>{
 
     });
 
